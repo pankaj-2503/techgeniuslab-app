7b70722e885c6f640081ce1551ee013ca30c0358
jesus7110 <juthikahazra11@gmail.com>
2023-11-24 17:55:49 +0530

    userController

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,3 +2,16 @@ const otpGenerator =require('otp-generator')
 
 const { User } = require('../models/userModel')
 
+const { Otp } = require('../models/otpModel')
+
+const jwt = require('jsonwebtoken');
+
+
+const generateAuthToken = (user) => {
+    const token = jwt.sign(
+        {
+            _id: user._id,
+            number: user.email,
+        }
+    )
+}
