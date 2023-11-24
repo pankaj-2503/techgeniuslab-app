5e1d767f8cff7369ee5a8ffcbeae5b7a0aee2670
jesus7110 <juthikahazra11@gmail.com>
2023-11-24 17:35:45 +0530

    otp number

--- Model/otpModel..js
+++ Model/otpModel..js
@@ -1,1 +1,12 @@
 const { Schema, model } = require("mongoose");
+
+
+module.exports.Otp = model('Otp', Schema({
+    number: {
+        type: String,
+        required: true
+    },
+   
+
+    // After 5 minutes it deleted automatically from the database
+}, { timestamps: true }))
