14257c9e9f3e5c9719ac424876840410e0186632
jesus7110 <juthikahazra11@gmail.com>
2023-11-24 17:36:38 +0530

    otp otp

--- Model/otpModel..js
+++ Model/otpModel..js
@@ -6,7 +6,11 @@ module.exports.Otp = model('Otp', Schema({
         type: String,
         required: true
     },
-   
+    otp: {
+        type: String,
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now, index: { expires: 300 } }
 
     // After 5 minutes it deleted automatically from the database
 }, { timestamps: true }))
