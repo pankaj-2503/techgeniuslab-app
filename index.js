4dbfceee8258a6b40af1be9b035a5a4a0abb1fee
jesus7110 <juthikahazra11@gmail.com>
2023-11-24 19:23:48 +0530

    Server JS

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express')
 require('dotenv').config();
 const connectDB = require("./config/database");
+const userRouter = require('./routers/userRouter');
+const productRouter = require('./routers/productRouter');
+const adminRouter = require('./routers/adminRouter');
+const orderRouter = require('./routers/orderRouter');
+const app = express()
