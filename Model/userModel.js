0eabaa94dc11d78ced2fdb3e25eeeb7c5d6e09e1
jesus7110 <juthikahazra11@gmail.com>
2023-11-24 16:04:12 +0530

    userchemma updated

--- Model/userModel.js
+++ Model/userModel.js
@@ -1,37 +1,54 @@
 const mongoose = require('mongoose');
+const jwt = require('')
+
+const userTypeValues = ['student', 'teacher', 'parent'];
 
 const userSchema = new mongoose.Schema({
-  username: { 
-    type: String, 
-    required: true },
-  email: { 
-    type: String, 
-    required: true },
-  password: { 
-    type: String, 
-    required: true },
-  userType: { 
-    type: String, 
-    required: true },
-  firstName: { 
-    type: String, 
-    required: true },
-  lastName: { 
-    type: String, 
-    required: true },
-  dateOfBirth: { 
-    type: Date, 
-    required: true },
-  grade: { 
-    type: Number, 
-    required: true },
-  schoolName: { 
-    type: String, 
-    required: true },
-  parentEmail: { 
-    type: String, 
-    required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true // Ensure uniqueness of email addresses
+  },
+  userType: {
+    type: String,
+    required: true,
+    enum: userTypeValues
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  },
+  schoolName: {
+    type: String,
+    required: true
+  },
+  parentEmail: {
+    type: String,
+    required: true
+  }
 });
+
+
+userSchema.methods.generateJWT = function () {
+    const token = jwt.sign({
+        _id: this._id,
+        number: this.number
+    }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+    return token
+}
+
 
 const User = mongoose.model('User', userSchema);
 
